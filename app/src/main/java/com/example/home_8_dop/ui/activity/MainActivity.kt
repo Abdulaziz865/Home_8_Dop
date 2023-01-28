9f62ba2a01676dcc86b25046b3aaaa40d196f885
Abdulaziz865 <111993898+Abdulaziz865@users.noreply.github.com>
2023-01-28 18:41:16 +0300

    Radin postav 10 ballow

---
+++ app/src/main/java/com/example/home_8_dop/ui/activity/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.home_8_dop.ui.activity
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.home_8_dop.R
+import com.example.home_8_dop.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val CHANNEL_ID = "channelID"
+        private const val CHANNEL_NAME = "channelName"
+        private const val NOTIF_ID = 0
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        createNotifChannel()
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        val notif = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Sample Title")
+            .setContentText("This is sample body notif")
+            .setSmallIcon(R.drawable.ic_baseline_info_24)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notifManger = NotificationManagerCompat.from(this)
+
+        binding.btnShowNotif.setOnClickListener {
+            notifManger.notify(NOTIF_ID, notif)
+        }
+    }
+
+    private fun createNotifChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                lightColor = Color.BLUE
+                enableLights(true)
+            }
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+}
